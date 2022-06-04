74f5deb4cff5c0ab269bdc8085e01af2a4024018
Thiago Dipold <thiagodipcavalcanti@gmail.com>
2022-06-04 11:44:45 -0300

    chore: add cors package

--- adote_um_pet/settings.py
+++ adote_um_pet/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'pet.apps.PetConfig',
     'adocao.apps.AdocaoConfig',
     'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -126,3 +128,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 APPEND_SLASH=False
+
+CORS_ALLOW_ALL_ORIGINS = True
