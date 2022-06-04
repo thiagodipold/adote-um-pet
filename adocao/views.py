930ea05e016919a99a70ae9aeb8ca7d53e98dbb1
Thiago Dipold <thiagodipcavalcanti@gmail.com>
2022-06-04 11:53:22 -0300

    feat: add custom object on error response

--- adocao/views.py
+++ adocao/views.py
@@ -9,4 +9,10 @@ class AdocaoList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=HTTP_201_CREATED)
-        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
+        return Response(
+            {
+                "errors": serializer.errors,
+                "message": "Houveram erros de validação."
+            },
+            status=HTTP_400_BAD_REQUEST
+        )
